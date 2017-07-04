2b3ba6dcc3e99cea75d4bf2b4e6cf0898d9a2615
Nicolas Petton <nicolas@petton.fr>
2017-07-04 18:03:55 +0200

    Use the new js-indent-align-list-continuation variable in Emacs trunk

--- emacs-js.el
+++ emacs-js.el
@@ -52,6 +52,8 @@
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
+(setq js-indent-align-list-continuation nil)
+
 (defun setup-js-buffer ()
   (setq mode-name "JS")
   (company-mode 1)
@@ -162,84 +164,6 @@ unreachable."
   (browse-url (format "https://developer.mozilla.org/en-US/search?q=%s&topic=js" searchString)))
 
 (define-key js-mode-map (kbd "C-c m") #'mdn-search)
-
-;;; modified version of js-proper-indentation that works with FTGP rules
-
-;; TODO: Commit a proper fix to Emacs.
-(defun js--proper-indentation (parse-status)
-  "Return the proper indentation for the current line."
-  (save-excursion
-    (back-to-indentation)
-    (cond ((nth 4 parse-status)    ; inside comment
-           (js--get-c-offset 'c (nth 8 parse-status)))
-          ((nth 3 parse-status) 0) ; inside string
-          ((eq (char-after) ?#) 0)
-          ((save-excursion (js--beginning-of-macro)) 4)
-          ;; Indent array comprehension continuation lines specially.
-          ((let ((bracket (nth 1 parse-status))
-                 beg)
-             (and bracket
-                  (not (js--same-line bracket))
-                  (setq beg (js--indent-in-array-comp bracket))
-                  ;; At or after the first loop?
-                  (>= (point) beg)
-                  (js--array-comp-indentation bracket beg))))
-          ((js--ctrl-statement-indentation))
-          ((js--multi-line-declaration-indentation))
-          ((nth 1 parse-status)
-           ;; A single closing paren/bracket should be indented at the
-           ;; same level as the opening statement. Same goes for
-           ;; "case" and "default".
-           (let ((same-indent-p (looking-at "[]})]"))
-                 (switch-keyword-p (looking-at "default\\_>\\|case\\_>[^:]"))
-                 (continued-expr-p (js--continued-expression-p)))
-             (goto-char (nth 1 parse-status)) ; go to the opening char
-             (if (looking-at "[({[]\\s-*\\(/[/*]\\|$\\)")
-                 (progn ; nothing following the opening paren/bracket
-                   (skip-syntax-backward " ")
-                   (when (eq (char-before) ?\)) (backward-list))
-                   (back-to-indentation)
-                   (let* ((in-switch-p (unless same-indent-p
-                                         (looking-at "\\_<switch\\_>")))
-                          (same-indent-p (or same-indent-p
-                                             (and switch-keyword-p
-                                                  in-switch-p)))
-                          (indent
-                           (cond (same-indent-p
-                                  (current-column))
-                                 (continued-expr-p
-                                  (+ (current-column) (* 2 js-indent-level)
-                                     js-expr-indent-offset))
-                                 (t
-                                  (+ (current-column) js-indent-level
-                                     (pcase (char-after (nth 1 parse-status))
-                                       (?\( js-paren-indent-offset)
-                                       (?\[ js-square-indent-offset)
-                                       (?\{ js-curly-indent-offset)))))))
-                     (if in-switch-p
-                         (+ indent js-switch-indent-offset)
-                       indent)))
-               ;; If there is something following the opening
-               ;; paren/bracket, everything else should be indented at
-               ;; the same level.
-               ; MODIFIED VERSION
-               (if same-indent-p
-                   (progn
-                     (back-to-indentation)
-                     (current-column))
-                 (progn
-                   (back-to-indentation)
-                   (+ (current-column) js2-basic-offset)))
-               ; COMMENTED ORIGINAL VERSION
-               ;; (unless same-indent-p
-               ;;   (forward-char)
-               ;;   (skip-chars-forward " \t"))
-               ;; (current-column)
-               )))
-
-          ((js--continued-expression-p)
-           (+ js-indent-level js-expr-indent-offset))
-          (t 0))))
 
 (add-to-list 'yas-snippet-dirs
              (expand-file-name "snippets"
