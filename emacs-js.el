0debe66e901e7573412f2ec25cdcd41a07f9c58b
Nicolas Petton <nicolas@petton.fr>
2020-05-03 13:46:06 +0200

    Fix indentation to match prettier

--- emacs-js.el
+++ emacs-js.el
@@ -74,9 +74,10 @@
 
   (setq-local compile-command "gulp")
 
-  (setq tab-width 4)
+  (setq tab-width 2)
   (setq indent-tabs-mode nil)
-  (setq js-switch-indent-offset 4)
+  (setq js-indent-level 2)
+  (setq js-switch-indent-offset 2)
 
   (setq js2-global-externs '("define" "require" "app" "fetch"))
   (setq js2-include-node-externs t)
